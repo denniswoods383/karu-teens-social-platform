5e89f4b5c634acf352767054db8130baa448a989
kali <kali@kali>
2025-08-21 17:04:14 +0300

    feat: Add notifications button to navbar
    🔔 Notifications Access:
    - Added bell icon button in navbar
    - Red notification badge indicator
    - Links to /notifications page
    - Positioned between stats and theme toggle
    
    Now users can access their notifications to see admin messages!

--- src/components/layout/EnhancedNavbar.tsx
+++ src/components/layout/EnhancedNavbar.tsx
@@ -143,6 +143,17 @@ export default function EnhancedNavbar() {
                 </div>
               </div>
 
+              {/* Notifications */}
+              <button
+                onClick={() => window.location.href = '/notifications'}
+                className="p-2 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-xl transition-colors duration-200 relative"
+                title="Notifications"
+              >
+                <span className="text-xl">🔔</span>
+                {/* Notification badge - you can add logic to show unread count */}
+                <span className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full"></span>
+              </button>
+
               {/* Theme Toggle */}
               <button
                 onClick={toggleTheme}
